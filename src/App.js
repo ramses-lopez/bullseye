ee9a84c0365aa8266497310aa0ce950d36e40ab6
Ramses Lopez <ramses.lopezs@gmail.com>
2018-01-13 16:28:57 -0400

    Added crosshair + movement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       hitCount: 0,
       errorCount: 0,
       targetPosition: {x:'50%', y:'50%'},
+      crossPosition: {x:'50%', y:'50%'},
+      crossStyle: {backgroundColor:''},
       showMessageBox: false
     }
   }
@@ -42,18 +44,65 @@ class App extends Component {
     }, 2000);
   }
 
+  moveCrosshair = e => {
+    const crosshair = document.querySelector('#crosshair')
+    const top = parseInt(crosshair.style.top.slice(0, -1))
+    const left = parseInt(crosshair.style.left.slice(0, -1))
+    const step = 7.5
+
+    switch (e.keyCode) {
+      case 32:
+        this.setState({crossStyle: {backgroundColor: 'red'}})
+        setTimeout(() => this.setState({crossStyle: {backgroundColor: ''}}), 50)
+        break;
+      case 37:
+        this.setState({crossPosition: {
+          x: ((left - step) + '%'),
+          y: this.state.crossPosition.y
+        }})
+        break;
+      case 38:
+        // up
+        this.setState({crossPosition: {
+          x: this.state.crossPosition.x,
+          y: ((top - step) + '%')
+        }})
+        break;
+      case 39:
+        // right
+        this.setState({crossPosition: {
+          x: ((left + step) + '%'),
+          y: this.state.crossPosition.y
+        }})
+        break;
+      case 40:
+        // down
+        this.setState({crossPosition: {
+          y: ((top + step) + '%'),
+          x: this.state.crossPosition.x
+        }})
+        break;
+      default:
+        //nothing
+    }
+  }
+
   componentDidMount(){
     this.moveTarget()
   }
 
   render() {
     return (
-      <div className='wrapper'>
+      <div className='wrapper' tabIndex={0} onKeyDown={this.moveCrosshair}>
         <h3 id="score">
           <span>miss {this.state.errorCount} </span>
           <span>hit {this.state.hitCount} </span>
           <a id="reset" type='button' value='reset' href="#" onClick={this.resetScore}>reset</a>
         </h3>
+        <div id="crosshair"
+          style={{backgroundColor: this.state.crossStyle.backgroundColor, top: this.state.crossPosition.y, left: this.state.crossPosition.x }}>
+          <span role="img" style={{width: 'fit-content'}}>❌</span>
+        </div>
         <div id="bullseye" onClick={this.countHits}
           style={{top: this.state.targetPosition.y,
             left: this.state.targetPosition.x }}
